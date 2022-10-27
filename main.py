628359ea6c8302f83dd3911d322f651c26a796d3
Mateus <mateuskienzle@gmail.com>
2022-10-27 20:16:53 -0300

    Add scrape of serie B and creation of data frame with data from the website

--- main.py
+++ main.py
@@ -12,11 +12,16 @@ from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
 
 
-teams_adress = {'palmeiras' : 'palmeiras/1963', 'internacional' : 'internacional/1966', 'flamengo' : 'flamengo/5981', 'fluminense' : 'fluminense/1961',
+teams_adress_A = {'palmeiras' : 'palmeiras/1963', 'internacional' : 'internacional/1966', 'flamengo' : 'flamengo/5981', 'fluminense' : 'fluminense/1961',
 'corinthians' : 'corinthians/1957', 'athletico paranaense' : 'athletico/1967', 'atletico mineiro' : 'atletico-mineiro/1977',
 'america mineiro' : 'america-mineiro/1973', 'fortaleza' : 'fortaleza/2020', 'botafogo' : 'botafogo/1958', 'santos' : 'santos/1968',
 'sao paulo' : 'sao-paulo/1981', 'bragantino' : 'red-bull-bragantino/1999', 'goias' : 'goias/1960', 'coritiba' : 'coritiba/1982',
 'ceara' : 'ceara/2001', 'cuiaba' : 'cuiaba/49202', 'atletico goianiense' : 'atletico-goianiense/7314', 'avai' : 'avai/7315', 'juventude' : 'juventude/1980'}
+
+teams_adress_B = {'cruzeiro' : 'cruzeiro/1954', 'gremio' : 'gremio/5926', 'vasco' : 'vasco-da-gama/1974', 'bahia' : 'bahia/1955', 'ituano' : 'ituano/2025',
+'londrina' : 'londrina/2022', 'sport' : 'sport-recife/1959', 'sampaio correa' : 'sampaio-correa/2005', 'criciuma' : 'criciuma/1984', 'crb' : 'crb/22032',
+'guarani' : 'guarani/1972', 'vila nova' : 'vila-nova/2021',  'ponte preta' : 'ponte-preta/1969', 'tombense' : 'tombense/87202', 'chapecoense' : 'chapecoense/21845',
+'csa' : 'csa/2010', 'novorizontino' : 'novorizontino/135514', 'brusque' : 'brusque-fc/21884', 'operario' : 'operario/39634', 'nautico' : 'nautico/2011'}
 
 base_url = 'https://www.sofascore.com/team/football/'
 
@@ -25,7 +30,7 @@ def team_search(time:str):
     
     driver = webdriver.Chrome(ChromeDriverManager().install())
     
-    url = base_url + teams_adress[time]
+    url = base_url + teams_adress_A[time]
 
     driver.get(url)
 
@@ -52,7 +57,7 @@ def team_search(time:str):
     
 # dictfinal = {}
 # listao = []
-# for key, value in teams_adress.items():
+# for key, value in teams_adress_A.items():
 #   listao.append(team_search(key))
 
 # dataframe = pd.DataFrame(listao[5].values(), listao[5].keys(), columns=['Valores'])
@@ -63,15 +68,7 @@ def team_search(time:str):
 browsers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 \(KHTML, like Gecko) Chrome / 86.0.4240.198Safari / 537.36"}
 
 base_api = 'https://api.sofascore.com/api/v1/team/'
-middle_api = '/unique-tournament/325/season/'
 end_api = '/statistics/overall'
-
-enpoint_17 = '13100'
-enpoint_18 = '16183'
-enpoint_19 = '22931'
-enpoint_20 = '27591'
-enpoint_21 = '36166'
-enpoint_22 = '40557'
 
 
 def escolhe_time(time:str):
@@ -80,7 +77,32 @@ def escolhe_time(time:str):
     cont_url_list = 0
     cont_data_list = 0
 
-    id_time = teams_adress[time][-4:]
+    division = input(str('Em qual divisão está o time?')).upper()
+
+    if division == 'A':
+        serie = '325'
+        id_time = teams_adress_A[time.lower()][-4:]
+        enpoint_17 = '13100'
+        enpoint_18 = '16183'
+        enpoint_19 = '22931'
+        enpoint_20 = '27591'
+        enpoint_21 = '36166'
+        enpoint_22 = '40557'
+
+    elif division == 'B':
+        serie = '390'
+        id_time = teams_adress_B[time.lower()][-4:]
+        enpoint_17 = ''
+        enpoint_18 = '16184'
+        enpoint_19 = '22932'
+        enpoint_20 = '27593'
+        enpoint_21 = '36162'
+        enpoint_22 = '40560'
+    
+
+
+
+    middle_api = f'/unique-tournament/{serie}/season/'
 
     url_17 = base_api + id_time + middle_api + enpoint_17 + end_api
     url_18 = base_api + id_time + middle_api + enpoint_18 + end_api
@@ -89,7 +111,11 @@ def escolhe_time(time:str):
     url_21 = base_api + id_time + middle_api + enpoint_21 + end_api
     url_22 = base_api + id_time + middle_api + enpoint_22 + end_api
 
-    urls_list = [url_17, url_18, url_19, url_20, url_21, url_22]
+    if division == 'A':
+        urls_list = [url_17, url_18, url_19, url_20, url_21, url_22]
+    if division == 'B':
+        urls_list = [url_17, url_18, url_19, url_20, url_21, url_22]
+
 
     for url in urls_list:
         api_link = requests.get(url, headers = browsers).json()
@@ -112,9 +138,21 @@ def escolhe_time(time:str):
 
     return data_list
 
+def build_dataframe(time:str): 
+    team = escolhe_time(time.lower())
+
+
+    team_dataframe = pd.DataFrame(index = team[0].keys())
+
+    for i in range(len(team)):
+        team_dataframe[str(team[i]['ano'])] = team[i].values()
+
+    team_dataframe['Media'] = team_dataframe.mean(axis=1)
+
+    return team_dataframe
 
 # pages_list = []
-# id_time = teams_adress['flamengo'][-4:]
+# id_time = teams_adress_A['flamengo'][-4:]
 # url_17 = base_api + id_time + middle_api + enpoint_17 + end_api
 # url_18 = base_api + id_time + middle_api + enpoint_18 + end_api
 # pages_list.append(requests.get(url_17, headers = browsers).json())
@@ -124,9 +162,10 @@ def escolhe_time(time:str):
    
 # Palmeiras
 
+#  https://api.sofascore.com/api/v1/team/ 1954    /unique-tournament/ 390 /season/    36162/  statistics/overall
 
-# 20/21
-# https://api.sofascore.com/api/v1/team/1963/unique-tournament/325/season/27591/statistics/overall
+# # 20/21
+#  https://api.sofascore.com/api/v1/team/ 1963    /unique-tournament/ 325 /season/    27591/  statistics/overall
 # https://api.sofascore.com/api/v1/unique-tournament/325/season/27591/top-teams/overall
 
 # 21
